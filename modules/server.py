0004a57a5edd4f25df8845648d9e696623116883
Mak Moskalenko <risknu@ukr.net>
2024-11-21 18:07:53 +0100

    Code refactor and messages content refactor
    yeah header is description too :)

--- modules/server.py
+++ modules/server.py
@@ -5,73 +5,79 @@ from disnake.ext import commands, tasks
 
 from typing import List
 
-from server_system import request_information, request_lenght, is_working, \
-    reconnect, request_lenght_names
+from server_system import (
+    request_information,
+    request_lenght,
+    is_working,
+    reconnect,
+    request_lenght_names,)
 
-class server(commands.Cog):
+class Server(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
-        self._bot: commands.Bot = bot
-        
+        self.bot: commands.Bot = bot
         self._update_status.start()
-        
+
     @tasks.loop(seconds=5)
     async def _update_status(self) -> None:
         reconnect()
+        length = request_lenght()
+        status = disnake.Status.online if is_working else disnake.Status.do_not_disturb
+        online_info = f"{'~' if length is None else length[0]}/{'~' if length is None else length[1]}"
         
-        lenght = request_lenght()
-        await self._bot.change_presence(
-            status=disnake.Status.online if is_working else disnake.Status.do_not_disturb, 
-            activity=disnake.Activity(type=disnake.ActivityType.watching,
-                name=f'online {"~" if lenght is None else lenght[0]}/{"~" if lenght is None else lenght[1]}'))
-        return None
+        await self.bot.change_presence(
+            status=status,
+            activity=disnake.Activity(
+                type=disnake.ActivityType.watching,
+                name=f"online {online_info}",))
 
     @_update_status.before_loop
     async def _before_update_status(self) -> None:
-        await self._bot.wait_until_ready()
-        return None
-        
+        await self.bot.wait_until_ready()
+
     @commands.Cog.listener("on_ready")
     async def _update_ready_status(self) -> None:
         reconnect()
+        length = request_lenght()
+        status = disnake.Status.online if is_working else disnake.Status.do_not_disturb
+        online_info = f"{'~' if length is None else length[0]}/{'~' if length is None else length[1]}"
         
-        lenght = request_lenght()
-        await self._bot.change_presence(
-            status=disnake.Status.online if is_working else disnake.Status.do_not_disturb, 
-            activity=disnake.Activity(type=disnake.ActivityType.watching,
-                name=f'online {"~" if lenght is None else lenght[0]}/{"~" if lenght is None else lenght[1]}'))
-        return None
-        
-    @commands.slash_command(name='online', description='it shows the servers online status')
-    async def _onl_slashcommand(self, iter: disnake.ApplicationCommandInteraction) -> None: 
+        await self.bot.change_presence(
+            status=status,
+            activity=disnake.Activity(
+                type=disnake.ActivityType.watching,
+                name=f"online {online_info}",))
+
+    @commands.slash_command(name="online", description="Displays the server's online status")
+    async def _online_status(self, inter: disnake.ApplicationCommandInteraction) -> None:
         if not is_working:
-            await iter.response.send_message("oops, it looks like something went wrong")
-            return None
+            await inter.response.send_message("Oops, it looks like something went wrong.")
+            return
         
         length = request_lenght()
         length_nicknames = request_lenght_names()
-        slashcommand_output: List[str] = []
-        
-        if length is not None:
-            slashcommand_output.append(f'current online count is {length[0]} out of {length[1]} players.\n')
-        if length_nicknames is not None:
-            slashcommand_output.append(f'nicknames of the players currently on the server are -> {", ".join(length_nicknames)}.')
+        output: List[str] = []
 
-        await iter.response.send_message("".join(slashcommand_output))
-        return None
-    
-    @commands.slash_command(name='informaion', description='it also displays server information such as version, protocol')
-    async def _inf_slashcommand(self, iter: disnake.ApplicationCommandInteraction) -> None: 
+        if length is not None:
+            output.append(f"Current online count: {length[0]} out of {length[1]} players.\n")
+        if length_nicknames is not None:
+            output.append(f"Players currently online: {', '.join(length_nicknames)}.")
+
+        await inter.response.send_message("".join(output))
+
+    @commands.slash_command(name="information", description="Displays server information such as version and protocol")
+    async def _server_information(self, inter: disnake.ApplicationCommandInteraction) -> None:
         if not is_working:
-            await iter.response.send_message("oops, it looks like something went wrong")
-            return None
+            await inter.response.send_message("Oops, it looks like something went wrong.")
+            return
         
-        info_tuple = request_information()
-        if info_tuple is None:
-            await iter.response.send_message("oops, it looks like something went wrong")
-            return None
+        info = request_information()
+        if info is None:
+            await inter.response.send_message("Oops, it looks like something went wrong.")
+            return
         
-        await iter.response.send_message(f"server version {info_tuple[0]} with protocol {info_tuple[1]}, players online: {info_tuple[2]}/{info_tuple[3]}")
-        return None
-    
+        version, protocol, current_players, max_players = info
+        await inter.response.send_message(
+            f"Server version: {version}, protocol: {protocol}, players online: {current_players}/{max_players}.")
+
 def setup(bot: commands.Bot) -> None:
-    bot.add_cog(server(bot))
+    bot.add_cog(Server(bot))
--- modules/wallet.py
+++ modules/wallet.py
@@ -4,66 +4,67 @@ import disnake
 from disnake.ext import commands
 
 from config import configuration
-from wallet_system import create_database_if_not_exists, create_account, \
-    request_balance, create_transaction, apply_transaction, \
-        transfer_money, request_transaction_status
+from wallet_system import (
+    create_database_if_not_exists,
+    create_account,
+    request_balance,
+    create_transaction,
+    apply_transaction,
+    transfer_money,
+    request_transaction_status,)
 
 create_database_if_not_exists()
-# TODO: check transaction status function
-class wallet(commands.Cog):
+
+# TODO: Implement transaction status checking
+class Wallet(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
-        self._bot: commands.Bot = bot
-    
-    @commands.slash_command(name="create_account", description="Create new account for shop")
-    async def _create_account(self, inter: disnake.ApplicationCommandInteraction) -> None:
+        self.bot: commands.Bot = bot
+
+    @commands.slash_command(name="create_account", description="Create a new account for the shop")
+    async def create_account_command(self, inter: disnake.ApplicationCommandInteraction) -> None:
         create_account(inter.author.id)
-        await inter.response.send_message("Your account successfuly created")
-        return None
-    
-    @commands.slash_command(name="wallet", description="View member wallet yeah")
-    async def _wallet_check(self, inter: disnake.ApplicationCommandInteraction, member: disnake.Member = None) -> None:
-        r_member: disnake.Member = member or inter.author
-        balance = request_balance(r_member.id)
+        await inter.response.send_message("Your account was successfully created.")
+
+    @commands.slash_command(name="wallet", description="View a member's wallet balance")
+    async def wallet_check(self, inter: disnake.ApplicationCommandInteraction, member: disnake.Member = None) -> None:
+        target_member: disnake.Member = member or inter.author
+        balance = request_balance(target_member.id)
         if balance is None:
-            await inter.response.send_message(f"Member {r_member.mention} has no account")
-            return None
+            await inter.response.send_message(f"Member {target_member.mention} does not have an account.")
         else:
-            await inter.response.send_message(f"Member {r_member.mention} has {balance} milc's")
-            return None
-    
-    @commands.slash_command(name="buy", description="Boy any product in our shop")
-    async def _buy_yeah(self, inter: disnake.ApplicationCommandInteraction, product_name: str, product_cost: int) -> None:
+            await inter.response.send_message(f"Member {target_member.mention} has `{balance}MILC(s)`.")
+
+    @commands.slash_command(name="buy", description="Purchase a product from the shop")
+    async def buy_product(self, inter: disnake.ApplicationCommandInteraction, product_name: str, product_cost: int) -> None:
         if product_cost <= 0:
-            await inter.response.send_message("Cost of product must be greater than 0")
-            return None
+            await inter.response.send_message("The product cost must be greater than 0.")
+            return
         
-        uuid: str = create_transaction(inter.author.id, product_name, product_cost)
-        await inter.response.send_message(f"Requiest to buy {product_name} created, with cost {product_cost} also UUID is {uuid}")
-        return None
-    
-    @commands.slash_command(name="apply_request", description="ADM | apply buy request")
-    async def _apply_transaction(self, inter: disnake.ApplicationCommandInteraction, uuid: str) -> None:
+        transaction_id: str = create_transaction(inter.author.id, product_name, product_cost)
+        await inter.response.send_message(
+            f"Purchase request created for `{product_name}` costing `{product_cost}`. Transaction ID: `{transaction_id}`")
+
+    @commands.slash_command(name="apply_request", description="Admin | Approve a purchase request")
+    async def apply_transaction_command(self, inter: disnake.ApplicationCommandInteraction, transaction_id: str) -> None:
         if inter.author.id in configuration.oids:
-            if apply_transaction(uuid):
-                await inter.response.send_message(f"Rquest to but with UUID {uuid} is seccusfuul")
-                return None
+            if apply_transaction(transaction_id):
+                await inter.response.send_message(f"Transaction with ID `{transaction_id}` has been successfully approved.")
             else:
-                await inter.response.send_message(f"Buy request wioth UUID {uuid} has declined")
-                return None
+                await inter.response.send_message(f"Transaction with ID `{transaction_id}` has been declined.")
         else:
-            await inter.response.send_message(f"{inter.author.mention} have no permissions")
-            return None
-        
-    @commands.slash_command(name="transactin", description="Accounts transaction")
-    async def _transaction(self, inter: disnake.ApplicationCommandInteraction, to_member: disnake.Member, size: int) -> None:
-        if size <= 0:
-            await inter.response.send_message("Size must be greater than 0")
-            return None
-        if transfer_money(inter.author.id, to_member.id, size):
-            await inter.response.send_message(f"Transaction amount {size} from member {inter.author.mention} to member {to_member.mention} successful")
-            return None
+            await inter.response.send_message(f"{inter.author.mention}, you do not have permission to perform this action.")
+
+    @commands.slash_command(name="transaction", description="Transfer money between accounts")
+    async def transfer_money_command(self, inter: disnake.ApplicationCommandInteraction, to_member: disnake.Member, amount: int) -> None:
+        if amount <= 0:
+            await inter.response.send_message("The transfer amount must be greater than 0.")
+            return
+
+        if transfer_money(inter.author.id, to_member.id, amount):
+            await inter.response.send_message(
+                f"Successfully transferred `{amount}MILC(s)` from {inter.author.mention} to {to_member.mention}.")
         else:
-            await inter.response.send_message("You dont have enough money man :(")
-        
+            await inter.response.send_message("You do not have enough funds for this transaction.")
+
 def setup(bot: commands.Bot) -> None:
-    bot.add_cog(wallet(bot))
+    bot.add_cog(Wallet(bot))
